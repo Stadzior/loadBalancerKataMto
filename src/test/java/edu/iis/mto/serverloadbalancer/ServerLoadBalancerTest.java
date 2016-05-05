f7a845fe48f453218595a6abdbb6278ed7dfc8b0
Adrian Pędziwiatr <dominiksu1910@gmail.com>
2016-05-05 18:46:20 +0200

    add red test for noVm

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -6,11 +6,21 @@ import static org.hamcrest.Matchers.equalTo;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 public class ServerLoadBalancerTest {
 	@Test
 	public void itCompiles() {
 		assertThat(true, equalTo(true));
 	}
 
+	@Test
+	public void balancigServer_noVm_ServerStaysEmpty(){
+		Server server = new ServerBuilder.withCapacity(1).build();
+		ServerLoadBalancer balancer = new ServerLoadBalancerBuilder.withVms(VmCollectionBuilder.Empty()).withServers(new ArrayList<Server>()).build();
+		balancer.balance();
+
+		assertThat(server, hasCurrentLoadOf(0.0d));
+	}
 
 }
