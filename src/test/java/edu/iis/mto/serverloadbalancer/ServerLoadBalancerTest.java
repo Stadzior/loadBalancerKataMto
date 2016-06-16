c8c232f247d22417fb34fc88570914dfa25988a3
Stadzior <Stadzior@gmail.com>
2016-06-16 13:09:40 +0200

    Red phase.

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -122,6 +122,41 @@ public class ServerLoadBalancerTest {
         assertThat(server.getVms().contains(vm3),equalTo(false));
     }
 
+    @Test
+    public void balancer_ShouldBalanceEightVms_OnThreeServers_DecliningAssignmentOfOneBigVm(){
+        Vm vm1 = new VmBuilder().withSize(1).build();
+        Vm vm2 = new VmBuilder().withSize(4).build();
+        Vm vm3 = new VmBuilder().withSize(1).build();
+        Vm vm4 = new VmBuilder().withSize(1).build();
+        Vm vm5 = new VmBuilder().withSize(4).build();
+        Vm vm6 = new VmBuilder().withSize(1).build();
+        Vm vm7 = new VmBuilder().withSize(100).build();
+        Vm vm8 = new VmBuilder().withSize(2).build();
+        Server server1 = new ServerBuilder().withCapacity(2).build();
+        Server server2 = new ServerBuilder().withCapacity(5).build();
+        Server server3 = new ServerBuilder().withCapacity(7).build();
+        ServerLoadBalancer balancer = new ServerLoadBalancerBuilder()
+                .withVms(new VmCollectionBuilder()
+                        .withVm(vm1)
+                        .withVm(vm2)
+                        .withVm(vm3)
+                        .withVm(vm4)
+                        .withVm(vm5)
+                        .withVm(vm6)
+                        .withVm(vm7)
+                        .build())
+                .withServers(new ServerCollectionBuilder()
+                        .withServer(server1)
+                        .withServer(server2)
+                        .withServer(server3)
+                        .build())
+                .build();
+        balancer.balance();
+        assertThat(server1.getVms().contains(vm1) && server1.getVms().contains(vm3) && server1.getVms().size() == 2,equalTo(true));
+        assertThat(server2.getVms().contains(vm2) && server1.getVms().contains(vm4) && server1.getVms().size() == 2,equalTo(true));
+        assertThat(server3.getVms().contains(vm5) && server1.getVms().contains(vm6)&& server1.getVms().contains(vm8) && server1.getVms().size() == 3,equalTo(true));
+    }
+
     private Matcher<? super Server> hasCurrentLoadOf(double load) {
         return new CurrentLoadPercentageMatcher(load);
     }
