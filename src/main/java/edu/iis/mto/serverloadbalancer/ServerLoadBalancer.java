50127f4c4d791281755e3ff320c68a21890052bf
Stadzior <Stadzior@gmail.com>
2016-06-16 12:36:57 +0200

    Green phase.

--- src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
@@ -4,18 +4,38 @@ import edu.iis.mto.serverloadbalancer.Collections.ServerCollection;
 import edu.iis.mto.serverloadbalancer.Collections.VmCollection;
 
 public class ServerLoadBalancer {
-
+    public static final double FULL_LOAD = 100.0d;
+    public static final double NO_LOAD = 0.0d;
     private VmCollection vms;
     private ServerCollection servers;
     public void balance() {
-        double sizeLeftOnServer;
         for(Vm vm : vms) {
-            for (Server server : servers) {
-                sizeLeftOnServer = (100.0d - server.getLoad())*server.getCapacity();
-                if(sizeLeftOnServer>vm.getSize())
-                    server.addVm(vm);
-            }
+                findLeastLoadedServerWithEnoughCapacity(servers, vm).addVm(vm);
         }
+    }
+
+    private Server findLeastLoadedServerWithEnoughCapacity(ServerCollection servers, Vm vm) {
+        double sizeLeftOnServer;
+        boolean serverHasEnoughCapacity;
+        ServerCollection capableServers = new ServerCollection();
+        for(Server server : servers){
+            sizeLeftOnServer = (100.0d - server.getLoad())*server.getCapacity();
+            serverHasEnoughCapacity = sizeLeftOnServer>vm.getSize();
+            if(serverHasEnoughCapacity)
+                capableServers.add(server);
+        }
+
+        return leastLoadedServer(capableServers);
+    }
+
+    private Server leastLoadedServer(ServerCollection capableServers) {
+        Server leastLoadedServer = new ServerBuilder().withCapacity(0).build();
+        leastLoadedServer.setLoad(FULL_LOAD);
+        for(Server server : capableServers){
+            if(server.getLoad()<leastLoadedServer.getLoad())
+                leastLoadedServer = server;
+        }
+        return leastLoadedServer;
     }
 
     public VmCollection getVms() {
