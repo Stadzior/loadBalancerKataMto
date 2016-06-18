a981dde1c3eafe0edff3779c7c3809d8c68bba5d
Stadzior <Stadzior@gmail.com>
2016-06-18 12:33:14 +0200

    Refactorized

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -3,7 +3,8 @@ package edu.iis.mto.serverloadbalancer;
 
 public class Server {
 
-	public double currentLoadPecentage;
+    private static final double MAXIMUM_LOAD = 100.0d;
+    public double currentLoadPecentage;
 	public int capacity;
 
 	public Server(int capacity) {
@@ -14,4 +15,7 @@ public class Server {
 		return true;
 	}
 
+    public void addVm(Vm vm) {
+        currentLoadPecentage = (double) vm.size / (double) capacity * MAXIMUM_LOAD;
+    }
 }
